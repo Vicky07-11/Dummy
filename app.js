14ce3b3041bb122d0b81fa18f5dd873537c30a24
VIGNESH <vvignesh8760@gmail.com>
2025-12-27 19:29:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,10 @@ function removeCurrentActives() {
     slide.classList.remove("active");
   });
 }
+
+let request = new XMLHttpRequest();
+request.open("GET","https://fakestoreapi.com/products");
+request.send();
+request.onload=() =>{
+    console.log(JSON.parse(request.response));
+}
